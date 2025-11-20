3d1ccfb8fceb861dc1d8baf367376bc9af3a72f8
MigoXV <108341045+MigoXV@users.noreply.github.com>
2025-11-20 13:59:43 +0800

    Refactor configuration into grouped dataclasses

---
+++ src/olah/application/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Copyright 2024 XiaHan
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+from .context import AppContext  # noqa: F401
+from .factory import create_application, get_app_context  # noqa: F401
